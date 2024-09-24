5faff150620773af18017ab01a0bcf374ebbad07
Aarij khan <aarij0100@gmail.com>
2024-09-25 01:30:37 +0500

    typewriter update

--- src/home.jsx
+++ src/home.jsx
@@ -3,15 +3,23 @@ import Navbar from "./navbar";
 import pic from "./assets/profile.jpeg";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Typewriter from 'typewriter-effect';
 import { useEffect } from "react";
 import { IoLogoCss3 } from "react-icons/io5";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { FaJs } from "react-icons/fa6";
 import { TbBrandReactNative } from "react-icons/tb";
 import { IoLogoFirebase } from "react-icons/io5";
-import { FaSquareGit ,FaLinkedin} from "react-icons/fa6";
+import { FaSquareGit, FaLinkedin } from "react-icons/fa6";
 import { SiNextdotjs } from "react-icons/si";
-import { FaGithub,FaWhatsappSquare ,FaPhoneAlt,FaHtml5,FaReact,FaBootstrap } from "react-icons/fa";
+import {
+  FaGithub,
+  FaWhatsappSquare,
+  FaPhoneAlt,
+  FaHtml5,
+  FaReact,
+  FaBootstrap,
+} from "react-icons/fa";
 import Footer from "./footer";
 import { Link } from "react-router-dom";
 function Home() {
@@ -23,7 +31,7 @@ function Home() {
     });
   }, []);
   const handleCall = () => {
-    window.location.href = 'tel:+92282183869'; 
+    window.location.href = "tel:+92282183869";
   };
 
   return (
@@ -35,29 +43,38 @@ function Home() {
           data-aos="fade-right"
         >
           <h1 className="font-poppins">Hi👋I am Aarij khan</h1>
-          <h1 className="font-poppins">Web & App Developer</h1>
+          {/* <h1 className="font-poppins">Web & App Developer</h1> */}
+          <h1 className="font-poppins mt-2">
+            <Typewriter
+              options={{
+                strings: ["Web & App Developer", "Software engineer"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h1>
           <div className="flex gap-2 sm:gap-10 ">
-            <Link to={'https://www.linkedin.com/in/aarij-raza-146364328'}>
-          <div className="flex items-center  focus:outline-none text-white bg-green-900 hover:bg-green-500 focus:ring-4 focus:ring-green-100 font-medium rounded-lg text-sm px-5 py-2.5  dark:focus:ring-green-600">
-              <FaLinkedin className="text-4xl" />
-            </div>
-            
-            
+            <Link to={"https://www.linkedin.com/in/aarij-raza-146364328"}>
+              <div className="flex items-center  focus:outline-none text-white bg-green-900 hover:bg-green-500 focus:ring-4 focus:ring-green-100 font-medium rounded-lg text-sm px-5 py-2.5  dark:focus:ring-green-600">
+                <FaLinkedin className="text-4xl" />
+              </div>
             </Link>
-            <Link to={'https://wa.me/923282183869'}>
-            <div className="flex items-center  focus:outline-none text-white bg-green-900 hover:bg-green-500 focus:ring-4 focus:ring-green-100 font-medium rounded-lg text-sm px-5 py-2.5  dark:focus:ring-green-600">
-              <FaWhatsappSquare className="text-4xl" />
-            </div>
+            <Link to={"https://wa.me/923282183869"}>
+              <div className="flex items-center  focus:outline-none text-white bg-green-900 hover:bg-green-500 focus:ring-4 focus:ring-green-100 font-medium rounded-lg text-sm px-5 py-2.5  dark:focus:ring-green-600">
+                <FaWhatsappSquare className="text-4xl" />
+              </div>
             </Link>
-            <div onClick={handleCall}  className="sm:hidden flex items-center  focus:outline-none text-white bg-green-900 hover:bg-green-500 focus:ring-4 focus:ring-green-100 font-medium rounded-lg text-sm px-5 py-2.5  dark:focus:ring-green-600">
+            <div
+              onClick={handleCall}
+              className="sm:hidden flex items-center  focus:outline-none text-white bg-green-900 hover:bg-green-500 focus:ring-4 focus:ring-green-100 font-medium rounded-lg text-sm px-5 py-2.5  dark:focus:ring-green-600"
+            >
               <FaPhoneAlt className="text-4xl" />
             </div>
-            <Link to={'https://github.com/Aarij-khan'}>
-            <div className="flex items-center  focus:outline-none text-white bg-green-900 hover:bg-green-500 focus:ring-4 focus:ring-green-100 font-medium rounded-lg text-sm px-5 py-2.5  dark:focus:ring-green-600">
-              <FaGithub className="text-4xl" />
-            </div>
+            <Link to={"https://github.com/Aarij-khan"}>
+              <div className="flex items-center  focus:outline-none text-white bg-green-900 hover:bg-green-500 focus:ring-4 focus:ring-green-100 font-medium rounded-lg text-sm px-5 py-2.5  dark:focus:ring-green-600">
+                <FaGithub className="text-4xl" />
+              </div>
             </Link>
-
           </div>
         </div>
         <div
@@ -71,37 +88,34 @@ function Home() {
           />
         </div>
       </div>
-      
-        <h1 className="uppercase text-4xl sm:text-6xl text-white font-poppins  text-center pb-5 mt-40">
-          Professional Skillset
-        </h1>
 
-        
-         <div className="pb-20 mt-10 w-full flex justify-center items-center flex-col bg-gradient-to-t from-green-900 to-green-600 overflow-x-hidden overflow-y-hidden"  >
-          <div className="flex gap-10 sm:flex-row flex-col sm:gap-32" >
-            <FaHtml5 className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3 " />
-            <IoLogoCss3 className="text-[200px]  border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
-            <RiTailwindCssFill className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3 " />
-          </div>
-          <div className="flex gap-10 sm:flex-row flex-col sm:gap-32 mt-10">
-            <FaJs className="text-[200px] border-2 text-black p-1 border-green-200 mt-10 sm:mt-3" />
-            <FaReact className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
-            <FaBootstrap className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
-          </div>
-          <div className="flex gap-10 sm:flex-row flex-col mt-10 sm:gap-32">
-            <FaGithub className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
-            <IoLogoFirebase className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
-            <FaSquareGit className="text-[200px] border-2 text-black shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
-          </div>
-          <div className="flex gap-10 sm:flex-row flex-col mt-10 sm:gap-32">
-            <TbBrandReactNative className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
-            <SiNextdotjs className="text-[200px] border-2 text-black shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
-          </div>
-        </div>
-        <Footer/>
+      <h1 className="uppercase text-4xl sm:text-6xl text-white font-poppins  text-center pb-5 mt-40">
+        Professional Skillset
+      </h1>
 
+      <div className="pb-20 mt-10 w-full flex justify-center items-center flex-col bg-gradient-to-t from-green-900 to-green-600 overflow-x-hidden overflow-y-hidden">
+        <div className="flex gap-10 sm:flex-row flex-col sm:gap-32">
+          <FaHtml5 className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3 " />
+          <IoLogoCss3 className="text-[200px]  border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
+          <RiTailwindCssFill className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3 " />
         </div>
-    
+        <div className="flex gap-10 sm:flex-row flex-col sm:gap-32 mt-10">
+          <FaJs className="text-[200px] border-2 text-black p-1 border-green-200 mt-10 sm:mt-3" />
+          <FaReact className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
+          <FaBootstrap className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
+        </div>
+        <div className="flex gap-10 sm:flex-row flex-col mt-10 sm:gap-32">
+          <FaGithub className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
+          <IoLogoFirebase className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
+          <FaSquareGit className="text-[200px] border-2 text-black shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
+        </div>
+        <div className="flex gap-10 sm:flex-row flex-col mt-10 sm:gap-32">
+          <TbBrandReactNative className="text-[200px] border-2 text-black  shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
+          <SiNextdotjs className="text-[200px] border-2 text-black shadow-2xl shadow-green-200 p-1 border-green-200 mt-3" />
+        </div>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
